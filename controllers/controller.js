7af6f347f3bd6411406e3a114b603d7cec0a3548
goBox <eauslander94@gmail.com>
2018-01-11 17:32:35 -0500

    All routes protected.  Authentication completed.

--- controllers/controller.js
+++ controllers/controller.js
@@ -50,6 +50,7 @@ const jwksRsa = require('jwks-rsa');
   // Sends back an observable emitting an array of fetched courts
   // Query param - Array of string _ids of courts to be fetched
   router.get('/getCourtsById', (req, res, next) => {
+
     courtModel.getCourtsById(JSON.parse(req.query.court_ids))
     .then((courts) => {
         let responseCourts = []
@@ -70,8 +71,9 @@ const jwksRsa = require('jwks-rsa');
     // 3 - Found no courts w/in courtside distance, sending back courts w/in 10 miles
     // 4 - No courts w/in 10 miles.  sends back only response code
 
-  router.get('/courtside', (req, res, next) => {
-    courtModel.courtsByLocation(JSON.parse(req.query.location), 100)
+  router.get('/courtside', checkJwt, (req, res, next) => {
+
+    courtModel.courtsByLocation(JSON.parse(req.get('location')), 100)
     .then((courts) => {
       switch(courts.length){
         case 1:
@@ -79,7 +81,7 @@ const jwksRsa = require('jwks-rsa');
           break;
         case 0:
         // get courts within 10 miles of location
-          courtModel.courtsByLocation(JSON.parse(req.query.location), 16000)
+          courtModel.courtsByLocation(JSON.parse(req.get('location')), 16000)
           .then((courts) => {
             if(courts.length > 0)
               res.send({responseCode: 3, courts: courts})
@@ -97,6 +99,7 @@ const jwksRsa = require('jwks-rsa');
   // Sends back an array of users
   // Param:  [String] - array of user ids of users to be fetched
   router.get('/getUsers', checkJwt, function(req, res, next) {
+
     // Resolve the promise returned by the model
     userModel.getUsers(JSON.parse(req.get('user_ids'))).then((userArrays) => {
       // massage data into array of users, send it back
@@ -110,7 +113,7 @@ const jwksRsa = require('jwks-rsa');
   // Sends back a single user corresponding to the auth_id provided
   // If there is no user with that id it sends back {}
   router.get('/getUsersByAuth_id', checkJwt, (req, res, next) => {
-    console.log("eli");
+
     userModel.getUsersByAuth_id(req.get('auth_id')).then((user) => {
       console.log(user);
       // If there are no users, send theempty object.  Else send the user
@@ -122,8 +125,9 @@ const jwksRsa = require('jwks-rsa');
 
 
   // Sends back an array of users that match the searchterm
-  router.get('/getUsersByName', function(req, res, next) {
-    userModel.getUsersByName(req.query.searchterm).then((userArrays) => {
+  router.get('/getUsersByName', checkJwt, function(req, res, next) {
+
+    userModel.getUsersByName(req.get('searchterm')).then((userArrays) => {
       // massage data into array of users, send it back
       let users = [];
       for(let user of userArrays)
@@ -153,14 +157,14 @@ const jwksRsa = require('jwks-rsa');
     courtModel.putWindowData(req.body.windowData).then((court) => {
       // emit that this window has been updated
       app.io.emit("windowUpdate" + court.windowData.court_id, court.windowData);
-      res.send(court);
+      res.send(court.windowData);
     }).catch((err) => {  next(err)  });
   })
 
 
   // Post: User in db is replaced with given user, updated user sent back in res objct
-  router.put('/putUser', (req, res, next) => {
-    console.log(req.body.user);
+  router.put('/putUser', checkJwt, (req, res, next) => {
+
     userModel.putUser(req.body.user).then((user) => {
       res.send(user);
     }).catch((err) => {  console.log(err);  next(err)  });
@@ -168,7 +172,8 @@ const jwksRsa = require('jwks-rsa');
 
 
   // Post: Sends back array of updated users, starting withuser 1
-  router.put('/addFriend', (req, res, next) => {
+  router.put('/addFriend', checkJwt, (req, res, next) => {
+
     userModel.addFriend(req.body.user1, req.body.user2).then((users, err) => {
       if (err) throw err;
       res.send([users[2], users[3]]);
@@ -177,7 +182,8 @@ const jwksRsa = require('jwks-rsa');
 
 
   // RequestedUser added to currentUser's friend requests
-  router.put('/requestFriend', (req, res, next) => {
+  router.put('/requestFriend', checkJwt, (req, res, next) => {
+
     userModel.requestFriend(req.body.requestedUser, req.body.currentUser)
      .then(() => { res.send({}) })
      .catch((err) => { console.log(err);  next(err)  });
@@ -185,9 +191,9 @@ const jwksRsa = require('jwks-rsa');
 
 
   // Sends back an array of the updated users, ser1 in position 0;
-  router.put('/removeFriend', (req, res, next) => {
+  router.put('/removeFriend', checkJwt, (req, res, next) => {
+
     userModel.removeFriend(req.body.user1, req.body.user2).then((users) =>{
-      console.log(users[0]);
       res.send([users[0], users[1]])
     }).catch((err) => {
       console.log('error in controller.js /removeFriend ' + err);
@@ -196,9 +202,11 @@ const jwksRsa = require('jwks-rsa');
   })
 
   // Post: Provided court_id is added to homecourt list of provided user_id
+  // In English, user gets a new homecourt
   // Param: court_id and user_id
   // Sends back: empty object
-  router.put('/putHomecourt', (req, res, next) => {
+  router.put('/putHomecourt', checkJwt, (req, res, next) => {
+
     userModel.putHomecourt(req.body.user_id, req.body.court_id).then(() => {
       res.send();
     }).catch((err) => {
@@ -211,8 +219,8 @@ const jwksRsa = require('jwks-rsa');
   // Post:  User added to court's pNow, court added to user's courtside
   // Body params: ids of respective user and court
   // Sends: updated court and updated user
-  router.put('/courtsidePut', (req, res, next) => {
-    console.log('got put request');
+  router.put('/courtsidePut', checkJwt, (req, res, next) => {
+
     let promises = [];
     promises.push(courtModel.courtsidePut(req.body.court_id, req.body.user_id));
     promises.push(userModel.courtsidePut(req.body.court_id, req.body.user_id));
@@ -229,7 +237,7 @@ const jwksRsa = require('jwks-rsa');
   // Param: The closure to be updated
   // Sends updated court object
   router.put('/putClosure', checkJwt, (req, res, next) => {
-    console.log(req.get('Authorization'));
+
     courtModel.putClosure(req.body.closure, req.body.court_id).then((data) => {
       res.send(data[1]);
     }).catch((err) => {
@@ -238,8 +246,8 @@ const jwksRsa = require('jwks-rsa');
   })
 
 
-  router.post('/newUser', (req, res, next) => {
-    console.log(req.body.user.nName);
+  router.post('/newUser', checkJwt, (req, res, next) => {
+
     userModel.newUser(req.body.user);
     next();
   })
@@ -248,23 +256,23 @@ const jwksRsa = require('jwks-rsa');
   // Param: the closure to be added and the court to wich it will be added
   // Sends back updated court
   router.post('/postClosure', checkJwt, (req, res, next) => {
-    console.log(req.get('Authorization'));
+
     courtModel.postClosure(req.body.closure, req.body.court_id).then((court) => {
       res.send(court);
     }).catch((err) => {
       console.log('err /postClosure in controller.js\n' +err)
-    });;
+    });
   })
 
 
   // Post:  User added to court's pNow, court added to user's courtside
   // Body params: ids of respective user and court
   // Sends: updated court and updated user
-  router.delete('/courtsideDelete', (req, res, next) => {
-    console.log(req.query.court_id + '\n' + req.query.user_id);
+  router.delete('/courtsideDelete', checkJwt, (req, res, next) => {
+
     let promises = [];
-    promises.push(courtModel.courtsideDelete(req.query.court_id, req.query.user_id));
-    promises.push(userModel.courtsideDelete(req.query.court_id, req.query.user_id));
+    promises.push(courtModel.courtsideDelete(req.get('court_id'), req.get('user_id')));
+    promises.push(userModel.courtsideDelete(req.get('court_id'), req.get('user_id')));
     Promise.all(promises).then((data) => {
       console.log(data)
       res.send({court: data[0], user: data[1]});
@@ -278,6 +286,7 @@ const jwksRsa = require('jwks-rsa');
   // Param: _id of the closure to be deleted
   // Sends back updated court
   router.delete('/deleteClosure', checkJwt, (req, res, next) => {
+
     courtModel.deleteClosure(req.get('closure_id'), req.get("court_id"))
     .then((court) => {
       res.send(court);
